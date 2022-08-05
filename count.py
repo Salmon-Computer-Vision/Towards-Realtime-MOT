5deace4131e74bc049680e91e6dfe07e8d9fddda
KamiCreed <9517086+KamiCreed@users.noreply.github.com>
2022-08-05 01:44:15 -0700

    Exit if reach last readable frame

--- count.py
+++ count.py
@@ -135,7 +135,7 @@ def eval_seq(opt, dataloader, data_type, result_filename, save_dir=None, show_im
     logger.info(f'Count threshold: {count_thresh}')
     for path, img, img0 in dataloader:
         if img0 is None:
-            continue
+            break
 
         if frame_id % 20 == 0:
             logger.info('Processing frame {} ({:.2f} fps)'.format(frame_id, 1./max(1e-5, timer.average_time)))
